db4b3d66c237de275a14bd0e44075df9059527c3
pomjak <pomjak71@gmail.com>
2023-03-25 19:22:56 +0100

    sql:smh

--- xpomsa00_xcagal00.sql
+++ xpomsa00_xcagal00.sql
@@ -9,13 +9,19 @@ DROP TABLE Reserved_rooms_acc CASCADE CONSTRAINTS;
 DROP TABLE Reserved_rooms_event CASCADE CONSTRAINTS;
 
 CREATE TABLE Customer (
-    customer_id INT NOT NULL,
-    name VARCHAR(100) NOT NULL,
+    personal_identification_number VARCHAR(11) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
     surname VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     phone VARCHAR(20) NOT NULL,
-    PRIMARY KEY (customer_id)
+    PRIMARY KEY (personal_identification_number)
 );
+
+ALTER TABLE Customer ADD CONSTRAINT PIN_check_regex
+CHECK (regexp_like(personal_identification_number, '^[0-9]{6}/[0-9]{4}$') );
+
+-- ALTER TABLE Customer ADD CONSTRAINT PIN_check_11
+-- CHECK(mod(personal_identification_number,11) = 0);
 
 CREATE TABLE Worker (
     worker_id INT NOT NULL ,
@@ -31,16 +37,15 @@ CREATE TABLE Reservation (
     reservation_type VARCHAR(20) NOT NULL,
     room_id INT,
     event_id INT,
-    customer_id INT NOT NULL,
+    personal_identification_number VARCHAR (11) NOT NULL,
     worker_id INT NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
     total_price DECIMAL(10, 2) NOT NULL,
     payment_status VARCHAR(20) NOT NULL,
     PRIMARY KEY (reservation_id),
-    -- FOREIGN KEY (room_id) REFERENCES Room(room_id),
-    FOREIGN KEY (worker_id) REFERENCES Worker(worker_id),
-    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+    CONSTRAINT worker_id FOREIGN KEY (worker_id) REFERENCES Worker(worker_id),
+    FOREIGN KEY (personal_identification_number) REFERENCES Customer(personal_identification_number)
 );
 
 CREATE TABLE Event (
@@ -69,10 +74,10 @@ CREATE TABLE Room_event (
     type VARCHAR(20) NOT NULL,          
     max_capacity INT ,
     area INT ,
-    customer_id INT,
+    personal_identification_number VARCHAR (11),
     event_id INT,
     PRIMARY KEY (room_id),
-    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id),
+    FOREIGN KEY (personal_identification_number) REFERENCES Customer(personal_identification_number),
     FOREIGN KEY (event_id) REFERENCES Event(event_id)
     );
 
@@ -83,9 +88,9 @@ CREATE TABLE Room_accommodation (
     single_beds INT ,
     double_beds INT ,
     class_luxury VARCHAR(20) NOT NULL,
-    customer_id INT,
+    personal_identification_number VARCHAR (11),
     PRIMARY KEY (room_id),
-    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+    FOREIGN KEY (personal_identification_number) REFERENCES Customer(personal_identification_number)
 );
 
 CREATE TABLE Reserved_rooms_acc (
