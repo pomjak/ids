5b798648f8c701f5b2f2e49ea401629f12f1ac28
pomjak <pomjak71@gmail.com>
2023-03-26 20:19:16 +0200

    clean code

--- xpomsa00_xcagal00.sql
+++ xpomsa00_xcagal00.sql
@@ -17,106 +17,115 @@ END;
 
 CREATE TABLE Customer (
     personal_id VARCHAR(10) NOT NULL
-    CONSTRAINT PIN_check_regex 
-    CHECK (REGEXP_LIKE(personal_id, '^[0-9]{6}[0-9]{4}$') AND NOT REGEXP_LIKE(personal_id, '^[0-9]{6}0000$')  and (MOD(personal_id,11) = 0)),
+                CONSTRAINT PIN_check_regex 
+                CHECK (REGEXP_LIKE(personal_id, '^[0-9]{6}[0-9]{4}$') AND NOT REGEXP_LIKE(personal_id, '^[0-9]{6}0000$')  and (MOD(personal_id,11) = 0)),
 
-    first_name VARCHAR(100) NOT NULL,
-    surname VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL 
-    CONSTRAINT mail_check_regex 
-    CHECK (regexp_like(email, '^[A-Za-z]+[A-Za-z0-9.]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$')),
+    first_name  VARCHAR(100) NOT NULL,
+    surname     VARCHAR(100) NOT NULL,
+    email       VARCHAR(100) NOT NULL 
+                CONSTRAINT mail_check_regex 
+                CHECK (regexp_like(email, '^[A-Za-z]+[A-Za-z0-9.]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$')),
     
-    phone VARCHAR(20) NOT NULL,
+    phone       VARCHAR(20) NOT NULL,
+
     PRIMARY KEY (personal_id)
 );
 
 CREATE TABLE Worker (
-    id INT GENERATED AS IDENTITY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL 
-    CONSTRAINT email_check_regex
-    CHECK (regexp_like(email, '^[A-Za-z]+[A-Za-z0-9.]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$')),
-    phone VARCHAR(20) NOT NULL,
-    position VARCHAR(100) NOT NULL,
+    id          INT GENERATED AS IDENTITY,
+    name        VARCHAR(100)    NOT NULL,
+    email       VARCHAR(100)    NOT NULL 
+                CONSTRAINT email_check_regex
+                CHECK (regexp_like(email, '^[A-Za-z]+[A-Za-z0-9.]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$')),
+    phone       VARCHAR(20)     NOT NULL,
+    position    VARCHAR(100)    NOT NULL,
+
     PRIMARY KEY (id)
 );
 
 
 CREATE TABLE Reservation (
-    id INT GENERATED AS IDENTITY ,
-    type VARCHAR(20) NOT NULL,
-    room_id INT,
-    event_id INT,
-    personal_id VARCHAR (10) NOT NULL,
-    worker_id INT NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    total_price DECIMAL(10, 2) NOT NULL,
-    payment_status VARCHAR(20) NOT NULL,
+    id              INT GENERATED AS IDENTITY ,
+    type            VARCHAR(20)     NOT NULL,
+    room_id         INT,
+    event_id        INT,
+    personal_id     VARCHAR (10)    NOT NULL,
+    worker_id       INT             NOT NULL,
+    start_date      DATE            NOT NULL,
+    end_date        DATE            NOT NULL,
+    total_price     DECIMAL(10, 2)  NOT NULL,
+    payment_status  VARCHAR(20)     NOT NULL,
+
     PRIMARY KEY (id),
     FOREIGN KEY (worker_id) REFERENCES Worker(id),
     FOREIGN KEY (personal_id) REFERENCES Customer(personal_id)
 );
 
 CREATE TABLE Event (
-    event_id INT GENERATED AS IDENTITY,
-    type VARCHAR(100) NOT NULL,
-    start_date DATE ,
-    end_date DATE ,
-    reservation_id INT NOT NULL,
+    event_id        INT GENERATED AS IDENTITY,
+    type            VARCHAR(100) NOT NULL,
+    start_date      DATE ,
+    end_date        DATE ,
+    reservation_id  INT NOT NULL,
+
     PRIMARY KEY (event_id),
     FOREIGN KEY (reservation_id) REFERENCES Reservation(id),
-    CONSTRAINT check_date CHECK (end_date > start_date)
+        CONSTRAINT check_date CHECK (end_date > start_date)
 );
 
 CREATE TABLE Service (
-    service_id INT GENERATED AS IDENTITY ,
-    name VARCHAR(100) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    reservation_id INT NOT NULL,
+    service_id      INT GENERATED AS IDENTITY ,
+    name            VARCHAR(100)    NOT NULL,
+    price           DECIMAL(10, 2)  NOT NULL,
+    reservation_id  INT NOT NULL,
+
     PRIMARY KEY (service_id),
     FOREIGN KEY (reservation_id) REFERENCES Reservation(id)
 );
 
 
--- Our ER diagram had Room for event and Room for accommodation generalized into one 'Room' entity, since they shared similar attributes, however in our implementation it made more sense 
--- to represent the generalization with distinct tables for each of them, as they are in relations with different entities
+-- Our ER diagram had Room for event and Room for accommodation generalized into one 'Room' entity, 
+-- since they shared similar attributes, however in our implementation it made more sense 
+-- to represent the generalization with distinct tables for each of them, 
+-- as they are in relations with different entities
 CREATE TABLE Room_event (
-    room_id INT NOT NULL ,
-    description VARCHAR(500) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    type VARCHAR(20) NOT NULL,          
-    max_capacity INT CONSTRAINT max CHECK( max_capacity > 0 ),
-    area INT CONSTRAINT area CHECK( area > 0 ),
-    personal_id VARCHAR (11) NOT NULL,
-    event_id INT NOT NULL,
+    room_id         INT             NOT NULL,
+    description     VARCHAR(500)    NOT NULL,
+    price           DECIMAL(10, 2)  NOT NULL,
+    type            VARCHAR(20)     NOT NULL,          
+    max_capacity    INT             CONSTRAINT max CHECK( max_capacity > 0 ),
+    area            INT             CONSTRAINT area CHECK( area > 0 ),
+    personal_id     VARCHAR (11)    NOT NULL,
+    event_id        INT             NOT NULL,
     PRIMARY KEY (room_id),
-    FOREIGN KEY (personal_id) REFERENCES Customer(personal_id),
-    FOREIGN KEY (event_id) REFERENCES Event(event_id)
+    FOREIGN KEY (personal_id)   REFERENCES Customer(personal_id),
+    FOREIGN KEY (event_id)      REFERENCES Event(event_id)
     );
 
 CREATE TABLE Room_accommodation (
-    room_id INT NOT NULL,
-    description VARCHAR(500),
-    price DECIMAL(10, 2) NOT NULL   ,
-    single_beds INT ,
-    double_beds INT ,
-    class_luxury VARCHAR(20) CONSTRAINT luxury_check CHECK( class_luxury IN ('Junior Suite','Deluxe Suite', 'Executive Suite', 'Terrace Suite') ),
-    personal_id VARCHAR (11) NOT NULL,
-    PRIMARY KEY (room_id),
+    room_id         INT             NOT NULL,
+    description     VARCHAR(500)            ,
+    price           DECIMAL(10, 2)  NOT NULL,
+    single_beds     INT                     ,
+    double_beds     INT                     ,
+    class_luxury    VARCHAR(20) CONSTRAINT luxury_check CHECK( class_luxury IN ('Junior Suite','Deluxe Suite', 'Executive Suite', 'Terrace Suite') ),
+    personal_id     VARCHAR (11)    NOT NULL,   
+    PRIMARY KEY (room_id)                   ,  
     FOREIGN KEY (personal_id) REFERENCES Customer(personal_id)
 );
 
 CREATE TABLE Reserved_rooms_acc (
-    reservation_id INT NOT NULL,
-    room_id INT NOT NULL ,
+    reservation_id  INT NOT NULL,
+    room_id         INT NOT NULL,
+
     FOREIGN KEY (reservation_id) REFERENCES Reservation(id),
     FOREIGN KEY (room_id) REFERENCES Room_accommodation(room_id)
 );
 
 CREATE TABLE Reserved_rooms_event (
-    reservation_id INT NOT NULL,
-    room_id INT NOT NULL ,
+    reservation_id  INT NOT NULL,
+    room_id         INT NOT NULL,
+
     FOREIGN KEY (reservation_id) REFERENCES Reservation(id),
     FOREIGN KEY (room_id) REFERENCES Room_event(room_id)
 );
